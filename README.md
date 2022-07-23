74c18412f8c88106380bc74e96e6b858ee32fe01
kyuhyong <kyuhyong@gmail.com>
2022-07-23 21:22:19 +0900

    Added services SetGoal and more

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# move_r1mini
+# move_r1mini for ROS-foxy
+
+move_r1mini is very simple position controller for R1mini mobile platform.
+
+This node will do followings
+
+- Open a service to receive a service call to "/set_goal"
+- Diff wheel controller will try to reach new goal position and angle requested
+- Publish Twist message via "/cmd_vel" topic
+
+This package is tested for R1mini platform.
+
+
+# Installation
+
+**tf-transformations** package is required and can be installed by
+```
+$ sudo apt install ros-<distro>-tf-transformations
+```
+
+Clone this packae to /src under a ros2 workspace
+```
+cd to ros2_ws/src
+git clone https://github.com/kyuhyong/move_r1mini.git
+cd ..
+colcon build --symlink-install
+```
+
+# Usage
+
+To move R1mini robot to X=1.0, Y=1.0 and goal yaw_z = 0.0
+Request a servcice call to set goal position and angle by entering below command
+```
+$ ros2 service call /set_goal move_commander_interface/srv/SetGoal "{'x':1.0,'y':1.0,'theta':0.0}"`
+```
+
+# To do
+
+Move controller is based on pure pursuit algorithm and is quite jittery. 
+May be improved by applying PID controllers
+
---
+++ move_commander/move_commander/move_commander_node.py
@@ -0,0 +1,104 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist, Pose, Point, Vector3, Quaternion, TransformStamped
+from nav_msgs.msg import Odometry
+from move_commander_interface.srv import SetGoal
+from tf_transformations import euler_from_quaternion
+import math
+
+GOAL_DIST_tolerance = 0.05
+GOAL_ANGLE_tolerance = 0.05
+
+MAX_Vx = 0.1        #maximum m/s
+MAX_Vw = 0.4        #maximum rad/s
+
+class OdomPose(object):
+    x = 0.0
+    y = 0.0
+    theta = 0.0
+
+class MoveCommanderNode(Node):
+    def __init__(self):
+        super().__init__('move_commander')
+        self.sub_odom = self.create_subscription(Odometry,'/odom', self.new_odom, 10 )
+        self.pub_cmdVel = self.create_publisher(Twist,'/cmd_vel', 10)
+        self.srv_setGoal = self.create_service(SetGoal, '/set_goal', self.new_setGoal)
+        timer_period = 0.1  # seconds
+        self.timer_count = 0
+        self.odom_new = OdomPose()
+        self.odom_origin = OdomPose()
+        self.odom_goal = OdomPose()
+        self.new_goal_set = False
+        self.goal_reached = False
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    def new_setGoal(self, request, response):
+        self.odom_goal.x = request.x
+        self.odom_goal.y = request.y
+        self.odom_goal.theta = request.theta
+        self.new_goal_set = True
+        self.goal_reached = False
+        print(f"New Goal: X= {self.odom_goal.x:.3f}, Y={self.odom_goal.y:.3f}, Theta={self.odom_goal.theta:.3f}")
+        return response
+
+    def timer_callback(self):
+        cmd_vel = Twist()
+        cmd_vel.linear.x = 0.0
+        cmd_vel.angular.z = 0.0
+        if self.new_goal_set and not self.goal_reached:
+            ## Compute delta x,y,angle to target
+            d_x_g = self.odom_goal.x - self.odom_new.x
+            d_y_g = self.odom_goal.y - self.odom_new.y
+            dist_goal = math.sqrt(d_x_g*d_x_g + d_y_g*d_y_g)
+            d_angle = self.odom_goal.theta - self.odom_new.theta
+
+            if dist_goal < GOAL_DIST_tolerance:
+                ## goal reached to target POSITION and finaly turn to target ANGLE
+                if abs(d_angle) < GOAL_ANGLE_tolerance:
+                    self.goal_reached = True
+                    self.new_goal_set = False
+                else:
+                    if d_angle > 0.0:
+                        cmd_vel.angular.z = MAX_Vw
+                    else:
+                        cmd_vel.angular.z = -MAX_Vw
+            else:
+                ## goal not reached to target POSITION
+                
+                # angle between current position to goal position relative to X axis
+                goal_angle = math.atan2(d_y_g, d_x_g)
+                d_angle = goal_angle - self.odom_new.theta
+                print(f"Dist:{dist_goal:.3f} D_angle:{goal_angle:.3f}")
+                if d_angle > 0.01:
+                    cmd_vel.angular.z = MAX_Vw
+                elif d_angle < -0.01:
+                    cmd_vel.angular.z = -MAX_Vw
+                if abs(d_angle) > 0.1:
+                    cmd_vel.linear.x = 0.0
+                else:
+                    cmd_vel.linear.x = MAX_Vx
+        self.pub_cmdVel.publish(cmd_vel)
+        self.timer_count+=1
+        #self.get_logger().info('Publishing: "%s"' % msg.data)
+
+    def new_odom(self, msg):
+        self.odom_new.x = msg.pose.pose.position.x
+        self.odom_new.y = msg.pose.pose.position.y
+        qx = msg.pose.pose.orientation.x
+        qy = msg.pose.pose.orientation.y
+        qz = msg.pose.pose.orientation.z
+        qw = msg.pose.pose.orientation.w
+        euler = euler_from_quaternion([qx,qy,qz,qw])
+        self.odom_new.theta = euler[2]
+        #print(f"theta={self.current_theta:.3f}")
+        #print(f"x={self.current_x},y={self.current_y},theta={self.current_theta}")
+
+def main(args=None):
+    rclpy.init(args=args)
+    move_commander_node = MoveCommanderNode()
+    rclpy.spin(move_commander_node)
+    move_commander_node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
